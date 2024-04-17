c216fc44aaeff1cf66e8522a699761fa2e490c89
pottabathinivivekananda <vivekananda.99666@gmail.com>
2024-04-17 21:45:00 +0530

    added demo

---
+++ src/demo/div.js
@@ -0,0 +1,19 @@
+import "./div.module.css"
+function Div(){
+    //this component is creating 3 divs
+    return(
+        <>
+            <div>
+                <h1>Hello</h1>
+            </div>
+            <div>
+                <h1>world</h1>
+            </div>
+            <div>
+                <h1>class</h1>
+            </div>
+        </>
+    )
+}
+
+export default Div;
